5f0dfcb7354f3b9e06c3a31ba29b6ed12f2870b8
Arminas Miliauskis <s032675@ad.viko.lt>
2021-04-26 16:24:42 +0300

    Login

--- TransportoNuoma/Form1.cs
+++ TransportoNuoma/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,7 +16,12 @@ namespace TransportoNuoma
         public Form1()
         {
             InitializeComponent();
+                
+           
         }
+
+        string connectionString = "server=34.91.29.158;user id=root;persistsecurityinfo=True;database=lsongulija";
+        SqlConnection con;
 
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -24,7 +30,29 @@ namespace TransportoNuoma
 
         private void loginButton_Click(object sender, EventArgs e)
         {
-
+            int count = 0;
+            try
+            {
+                con = new SqlConnection(connectionString);
+                con.Open();
+                SqlCommand cmd1 = con.CreateCommand();
+                cmd1.CommandType = CommandType.Text;
+                cmd1.CommandText = " select * from klientas where email = '" + loginEmail.Text + "' and slaptazodis = '" + loginPassword.Text + "'";
+                cmd1.ExecuteNonQuery();
+                DataTable dt1 = new DataTable();
+                SqlDataAdapter da1 = new SqlDataAdapter(cmd1);
+                da1.Fill(dt1);
+                count = Convert.ToInt32(dt1.Rows.Count.ToString());
+                if (count == 0)
+                {
+                    MessageBox.Show("Vartotojas neegzistuoja");
+                }
+            
+                else { MessageBox.Show("OK"); }
+            //MessageBox.Show("OK");
+                con.Close();
+            }catch(Exception ex) { MessageBox.Show(ex.Message); }         
+            
         }
 
         private void registerButton_Click(object sender, EventArgs e)
