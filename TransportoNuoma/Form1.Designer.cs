34c22d02ccb0068f60922c58eed7fd796f4dacc2
PC <lsongulija@gmail.com>
2021-04-26 15:28:43 +0200

    first commit

---
+++ TransportoNuoma/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace TransportoNuoma
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loginButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.loginEmail = new System.Windows.Forms.TextBox();
+            this.loginPassword = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.registerPassword = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.registerEmail = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.registerButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // loginButton
+            // 
+            this.loginButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.loginButton.Location = new System.Drawing.Point(108, 232);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(135, 38);
+            this.loginButton.TabIndex = 0;
+            this.loginButton.Text = "Prisijungti";
+            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(105, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(175, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Prisijungimas";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(111, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Email";
+            // 
+            // loginEmail
+            // 
+            this.loginEmail.Location = new System.Drawing.Point(108, 128);
+            this.loginEmail.Name = "loginEmail";
+            this.loginEmail.Size = new System.Drawing.Size(172, 20);
+            this.loginEmail.TabIndex = 3;
+            // 
+            // loginPassword
+            // 
+            this.loginPassword.Location = new System.Drawing.Point(108, 190);
+            this.loginPassword.Name = "loginPassword";
+            this.loginPassword.Size = new System.Drawing.Size(172, 20);
+            this.loginPassword.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(111, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Password";
+            // 
+            // registerPassword
+            // 
+            this.registerPassword.Location = new System.Drawing.Point(417, 190);
+            this.registerPassword.Name = "registerPassword";
+            this.registerPassword.Size = new System.Drawing.Size(172, 20);
+            this.registerPassword.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(420, 161);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 20);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Password";
+            // 
+            // registerEmail
+            // 
+            this.registerEmail.Location = new System.Drawing.Point(417, 128);
+            this.registerEmail.Name = "registerEmail";
+            this.registerEmail.Size = new System.Drawing.Size(172, 20);
+            this.registerEmail.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(420, 99);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Email";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(414, 46);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(157, 31);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Registracija";
+            // 
+            // registerButton
+            // 
+            this.registerButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.registerButton.Location = new System.Drawing.Point(417, 232);
+            this.registerButton.Name = "registerButton";
+            this.registerButton.Size = new System.Drawing.Size(135, 38);
+            this.registerButton.TabIndex = 6;
+            this.registerButton.Text = "Registruotis";
+            this.registerButton.UseVisualStyleBackColor = true;
+            this.registerButton.Click += new System.EventHandler(this.registerButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.registerPassword);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.registerEmail);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.registerButton);
+            this.Controls.Add(this.loginPassword);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.loginEmail);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.loginButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox loginEmail;
+        private System.Windows.Forms.TextBox loginPassword;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox registerPassword;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox registerEmail;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button registerButton;
+    }
+}
+
