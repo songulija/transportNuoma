21fcb7c571b3c15bdf04f4244222646cecec9b34
Arminas Miliauskis <s032675@ad.viko.lt>
2021-04-27 10:56:21 +0300

    Fixed Login

--- TransportoNuoma/Form1.cs
+++ TransportoNuoma/Form1.cs
@@ -1,27 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Data;
-using System.Data.SqlClient;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using TransportoNuoma.Classes;
+using TransportoNuoma.Repositories;
 
 namespace TransportoNuoma
 {
     public partial class Form1 : Form
     {
+        UsersRepository usersRepository;
         public Form1()
         {
             InitializeComponent();
-                
-           
+
+            usersRepository = new UsersRepository();
         }
 
-        string connectionString = "server=34.91.29.158;user id=root;persistsecurityinfo=True;database=lsongulija";
-        SqlConnection con;
+        static string connectionString = "server=34.91.29.158;user id=root;persistsecurityinfo=True;port=3306;database=lsongulija;password=123456";
+        MySqlConnection con = new MySqlConnection(connectionString);
 
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -30,43 +27,62 @@ namespace TransportoNuoma
 
         private void loginButton_Click(object sender, EventArgs e)
         {
-            
-            int count = 0;
-            try
+            string selectString =
+            "SELECT Email, Slaptazodis " +
+            "FROM klientas " +
+            "WHERE Email = '" + loginEmail.Text + "' AND Slaptazodis = '" + loginPassword.Text + "'";
+
+            MySqlCommand mySqlCommand = new MySqlCommand(selectString, con);
+            con.Open();
+            String strResult = String.Empty;
+            strResult = (String)mySqlCommand.ExecuteScalar();
+            con.Close();
+
+            if (strResult.Length == 0)
             {
-                //establishes new connection to the database
-                con = new SqlConnection(connectionString);
-                con.Open();
-                //creates an sql command to the connected database
-                SqlCommand cmd1 = con.CreateCommand();
-                //definies the command type
-                cmd1.CommandType = CommandType.Text;
-                //selects everything from client table where email and password is same as those written in login textboxes
-                cmd1.CommandText = " select * from klientas where email = '" + loginEmail.Text + "' and slaptazodis = '" + loginPassword.Text + "'";
-                //executes the command
-                cmd1.ExecuteNonQuery();
-                //creates new datatable
-                DataTable dt1 = new DataTable();               
-                SqlDataAdapter da1 = new SqlDataAdapter(cmd1);
-                //fill the datatable with datarows from the database
-                da1.Fill(dt1);
-                //sets count to one if theres a data row with email and password specified in the login textboxes
-                count = Convert.ToInt32(dt1.Rows.Count.ToString());
-                if (count == 0)
-                {
-                    MessageBox.Show("Vartotojas neegzistuoja");
-                }
-            
-                else { MessageBox.Show("OK"); }
-            
-                con.Close();
-            }catch(Exception ex) { MessageBox.Show(ex.Message); }         
-            
+                MessageBox.Show("Incorrect email or password");
+            //could redirect to register page or forget email/pass page
+}
+            else
+            {
+                MessageBox.Show("You are logged in");
+                // open post loggin form
+            }
+
         }
 
         private void registerButton_Click(object sender, EventArgs e)
         {
+            try
+            {   
+                if(repeatPassword.Text == registerPassword.Text)
+                {
+                    //create student object out of textbox values
+                    Klientas klientas = new Klientas();
+                    klientas.id = 16;
+                    klientas.vardas = vardasRegister.Text;
+                    klientas.pavarde = pavardeRegister.Text;
+                    klientas.kodas = int.Parse(kodasRegister.Text);
+                    klientas.email = registerEmail.Text;
+                    klientas.slaptazodis = registerPassword.Text;
+                    Klientas registerClient = usersRepository.RegisterClient(klientas);
+                    if (registerClient.vardas != null && registerClient.vardas != "")
+                    {
+                        MessageBox.Show("Register");
+                    }
+                }
 
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.ToString());
+            }
+            
+        }
+
+        private void getClients_Click(object sender, EventArgs e)
+        {
+            usersRepository.displayClients();
         }
     }
 }
