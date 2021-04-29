c08677202343b95dbe1c1627e96cac0cd317bfb1
PC <lsongulija@gmail.com>
2021-04-29 13:34:54 +0200

    AdminKlientForm register and update added

--- TransportoNuoma/AdminKlientasForm.Designer.cs
+++ TransportoNuoma/AdminKlientasForm.Designer.cs
@@ -28,12 +28,350 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.TransportPanel = new System.Windows.Forms.Panel();
+            this.updateKlientaiPanel = new System.Windows.Forms.Panel();
+            this.updateKlientas = new System.Windows.Forms.Button();
+            this.addKlientaiPanel = new System.Windows.Forms.Panel();
+            this.addKlientas = new System.Windows.Forms.Button();
+            this.addKlientasSlapt = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.addKlientasEmail = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.addKlientasAsmKodas = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.addKlientasPavarde = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.addKlientasVardas = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.updateKlientaiShow = new System.Windows.Forms.Button();
+            this.AddKlientaiShow = new System.Windows.Forms.Button();
+            this.getKlientai = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.updateKlientasEmail = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.updateKlientasPav = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.updateKlientasVardas = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.updateKlientasKlientoNr = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.TransportPanel.SuspendLayout();
+            this.updateKlientaiPanel.SuspendLayout();
+            this.addKlientaiPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TransportPanel
+            // 
+            this.TransportPanel.Controls.Add(this.updateKlientaiPanel);
+            this.TransportPanel.Controls.Add(this.addKlientaiPanel);
+            this.TransportPanel.Controls.Add(this.updateKlientaiShow);
+            this.TransportPanel.Controls.Add(this.AddKlientaiShow);
+            this.TransportPanel.Controls.Add(this.getKlientai);
+            this.TransportPanel.Controls.Add(this.dataGridView1);
+            this.TransportPanel.Controls.Add(this.label1);
+            this.TransportPanel.Location = new System.Drawing.Point(12, 12);
+            this.TransportPanel.Name = "TransportPanel";
+            this.TransportPanel.Size = new System.Drawing.Size(382, 631);
+            this.TransportPanel.TabIndex = 1;
+            // 
+            // updateKlientaiPanel
+            // 
+            this.updateKlientaiPanel.Controls.Add(this.updateKlientasKlientoNr);
+            this.updateKlientaiPanel.Controls.Add(this.updateKlientasEmail);
+            this.updateKlientaiPanel.Controls.Add(this.label10);
+            this.updateKlientaiPanel.Controls.Add(this.label7);
+            this.updateKlientaiPanel.Controls.Add(this.updateKlientasPav);
+            this.updateKlientaiPanel.Controls.Add(this.label8);
+            this.updateKlientaiPanel.Controls.Add(this.updateKlientasVardas);
+            this.updateKlientaiPanel.Controls.Add(this.label9);
+            this.updateKlientaiPanel.Controls.Add(this.updateKlientas);
+            this.updateKlientaiPanel.Location = new System.Drawing.Point(204, 248);
+            this.updateKlientaiPanel.Name = "updateKlientaiPanel";
+            this.updateKlientaiPanel.Size = new System.Drawing.Size(178, 363);
+            this.updateKlientaiPanel.TabIndex = 17;
+            this.updateKlientaiPanel.Visible = false;
+            // 
+            // updateKlientas
+            // 
+            this.updateKlientas.Location = new System.Drawing.Point(20, 180);
+            this.updateKlientas.Name = "updateKlientas";
+            this.updateKlientas.Size = new System.Drawing.Size(126, 33);
+            this.updateKlientas.TabIndex = 14;
+            this.updateKlientas.Text = "Update";
+            this.updateKlientas.UseVisualStyleBackColor = true;
+            this.updateKlientas.Click += new System.EventHandler(this.updateKlientas_Click);
+            // 
+            // addKlientaiPanel
+            // 
+            this.addKlientaiPanel.Controls.Add(this.addKlientas);
+            this.addKlientaiPanel.Controls.Add(this.addKlientasSlapt);
+            this.addKlientaiPanel.Controls.Add(this.label6);
+            this.addKlientaiPanel.Controls.Add(this.addKlientasEmail);
+            this.addKlientaiPanel.Controls.Add(this.label5);
+            this.addKlientaiPanel.Controls.Add(this.addKlientasAsmKodas);
+            this.addKlientaiPanel.Controls.Add(this.label4);
+            this.addKlientaiPanel.Controls.Add(this.addKlientasPavarde);
+            this.addKlientaiPanel.Controls.Add(this.label3);
+            this.addKlientaiPanel.Controls.Add(this.addKlientasVardas);
+            this.addKlientaiPanel.Controls.Add(this.label2);
+            this.addKlientaiPanel.Location = new System.Drawing.Point(18, 249);
+            this.addKlientaiPanel.Name = "addKlientaiPanel";
+            this.addKlientaiPanel.Size = new System.Drawing.Size(178, 363);
+            this.addKlientaiPanel.TabIndex = 5;
+            this.addKlientaiPanel.Visible = false;
+            // 
+            // addKlientas
+            // 
+            this.addKlientas.Location = new System.Drawing.Point(20, 212);
+            this.addKlientas.Name = "addKlientas";
+            this.addKlientas.Size = new System.Drawing.Size(126, 33);
+            this.addKlientas.TabIndex = 14;
+            this.addKlientas.Text = "Add";
+            this.addKlientas.UseVisualStyleBackColor = true;
+            this.addKlientas.Click += new System.EventHandler(this.addKlientas_Click);
+            // 
+            // addKlientasSlapt
+            // 
+            this.addKlientasSlapt.Location = new System.Drawing.Point(20, 186);
+            this.addKlientasSlapt.Name = "addKlientasSlapt";
+            this.addKlientasSlapt.Size = new System.Drawing.Size(126, 20);
+            this.addKlientasSlapt.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(17, 169);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(61, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Slaptazodis";
+            // 
+            // addKlientasEmail
+            // 
+            this.addKlientasEmail.Location = new System.Drawing.Point(20, 146);
+            this.addKlientasEmail.Name = "addKlientasEmail";
+            this.addKlientasEmail.Size = new System.Drawing.Size(126, 20);
+            this.addKlientasEmail.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(17, 129);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Email";
+            // 
+            // addKlientasAsmKodas
+            // 
+            this.addKlientasAsmKodas.Location = new System.Drawing.Point(20, 106);
+            this.addKlientasAsmKodas.Name = "addKlientasAsmKodas";
+            this.addKlientasAsmKodas.Size = new System.Drawing.Size(126, 20);
+            this.addKlientasAsmKodas.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 89);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Asmens_Kodas";
+            // 
+            // addKlientasPavarde
+            // 
+            this.addKlientasPavarde.Location = new System.Drawing.Point(20, 66);
+            this.addKlientasPavarde.Name = "addKlientasPavarde";
+            this.addKlientasPavarde.Size = new System.Drawing.Size(126, 20);
+            this.addKlientasPavarde.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Pavarde";
+            // 
+            // addKlientasVardas
+            // 
+            this.addKlientasVardas.Location = new System.Drawing.Point(20, 26);
+            this.addKlientasVardas.Name = "addKlientasVardas";
+            this.addKlientasVardas.Size = new System.Drawing.Size(126, 20);
+            this.addKlientasVardas.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Vardas";
+            // 
+            // updateKlientaiShow
+            // 
+            this.updateKlientaiShow.Location = new System.Drawing.Point(202, 219);
+            this.updateKlientaiShow.Name = "updateKlientaiShow";
+            this.updateKlientaiShow.Size = new System.Drawing.Size(110, 23);
+            this.updateKlientaiShow.TabIndex = 4;
+            this.updateKlientaiShow.Text = "Update Klientai";
+            this.updateKlientaiShow.UseVisualStyleBackColor = true;
+            this.updateKlientaiShow.Click += new System.EventHandler(this.updateKlientaiShow_Click);
+            // 
+            // AddKlientaiShow
+            // 
+            this.AddKlientaiShow.Location = new System.Drawing.Point(110, 219);
+            this.AddKlientaiShow.Name = "AddKlientaiShow";
+            this.AddKlientaiShow.Size = new System.Drawing.Size(86, 23);
+            this.AddKlientaiShow.TabIndex = 3;
+            this.AddKlientaiShow.Text = "Add Klientai";
+            this.AddKlientaiShow.UseVisualStyleBackColor = true;
+            this.AddKlientaiShow.Click += new System.EventHandler(this.AddKlientaiShow_Click);
+            // 
+            // getKlientai
+            // 
+            this.getKlientai.Location = new System.Drawing.Point(18, 220);
+            this.getKlientai.Name = "getKlientai";
+            this.getKlientai.Size = new System.Drawing.Size(86, 23);
+            this.getKlientai.TabIndex = 2;
+            this.getKlientai.Text = "Get Klientai";
+            this.getKlientai.UseVisualStyleBackColor = true;
+            this.getKlientai.Click += new System.EventHandler(this.getKlientai_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(18, 42);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(351, 171);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(14, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Klientai";
+            // 
+            // updateKlientasEmail
+            // 
+            this.updateKlientasEmail.Location = new System.Drawing.Point(20, 107);
+            this.updateKlientasEmail.Name = "updateKlientasEmail";
+            this.updateKlientasEmail.Size = new System.Drawing.Size(126, 20);
+            this.updateKlientasEmail.TabIndex = 20;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(17, 90);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(32, 13);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "Email";
+            // 
+            // updateKlientasPav
+            // 
+            this.updateKlientasPav.Location = new System.Drawing.Point(20, 67);
+            this.updateKlientasPav.Name = "updateKlientasPav";
+            this.updateKlientasPav.Size = new System.Drawing.Size(126, 20);
+            this.updateKlientasPav.TabIndex = 18;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(17, 50);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(47, 13);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Pavarde";
+            // 
+            // updateKlientasVardas
+            // 
+            this.updateKlientasVardas.Location = new System.Drawing.Point(20, 27);
+            this.updateKlientasVardas.Name = "updateKlientasVardas";
+            this.updateKlientasVardas.Size = new System.Drawing.Size(126, 20);
+            this.updateKlientasVardas.TabIndex = 16;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(17, 10);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(40, 13);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "Vardas";
+            // 
+            // updateKlientasKlientoNr
+            // 
+            this.updateKlientasKlientoNr.Location = new System.Drawing.Point(20, 147);
+            this.updateKlientasKlientoNr.Name = "updateKlientasKlientoNr";
+            this.updateKlientasKlientoNr.Size = new System.Drawing.Size(126, 20);
+            this.updateKlientasKlientoNr.TabIndex = 16;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(17, 130);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(50, 13);
+            this.label10.TabIndex = 15;
+            this.label10.Text = "KlientoNr";
+            // 
+            // AdminKlientasForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1244, 654);
+            this.Controls.Add(this.TransportPanel);
+            this.Name = "AdminKlientasForm";
             this.Text = "AdminKlientasForm";
+            this.TransportPanel.ResumeLayout(false);
+            this.TransportPanel.PerformLayout();
+            this.updateKlientaiPanel.ResumeLayout(false);
+            this.updateKlientaiPanel.PerformLayout();
+            this.addKlientaiPanel.ResumeLayout(false);
+            this.addKlientaiPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel TransportPanel;
+        private System.Windows.Forms.Panel updateKlientaiPanel;
+        private System.Windows.Forms.Button updateKlientas;
+        private System.Windows.Forms.Panel addKlientaiPanel;
+        private System.Windows.Forms.Button addKlientas;
+        private System.Windows.Forms.TextBox addKlientasSlapt;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox addKlientasEmail;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox addKlientasAsmKodas;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox addKlientasPavarde;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox addKlientasVardas;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button updateKlientaiShow;
+        private System.Windows.Forms.Button AddKlientaiShow;
+        private System.Windows.Forms.Button getKlientai;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox updateKlientasEmail;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox updateKlientasPav;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox updateKlientasVardas;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox updateKlientasKlientoNr;
+        private System.Windows.Forms.Label label10;
     }
 }
--- TransportoNuoma/AdminKlientasForm.cs
+++ TransportoNuoma/AdminKlientasForm.cs
@@ -27,5 +27,92 @@ namespace TransportoNuoma
             galimiNuzRep = new GalimiNusizengimaiRepository();
             nusizRep = new NusizengimaiRepository();
         }
+
+        private void getKlientai_Click(object sender, EventArgs e)
+        {
+            getKlientaiDisplay();
+        }
+
+        private void AddKlientaiShow_Click(object sender, EventArgs e)
+        {
+            updateKlientaiPanel.Visible = false;
+            addKlientaiPanel.Visible = true;
+        }
+
+        private void updateKlientaiShow_Click(object sender, EventArgs e)
+        {
+            addKlientaiPanel.Visible = false;
+            updateKlientaiPanel.Visible = true;
+        }
+
+        private void addKlientas_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Klientas kl = new Klientas();
+                kl.vardas = addKlientasVardas.Text;
+                kl.pavarde = addKlientasPavarde.Text;
+                kl.email = addKlientasEmail.Text;
+                kl.kodas = int.Parse(addKlientasAsmKodas.Text);
+                kl.slaptazodis = addKlientasSlapt.Text;
+
+                Klientas insertKl = usersRep.RegisterClient(kl);
+
+                addKlientasVardas.Clear();
+                addKlientasPavarde.Clear();
+                addKlientasEmail.Clear();
+                addKlientasAsmKodas.Clear();
+                addKlientasSlapt.Clear();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            MessageBox.Show("Succesfully inserted");
+            getKlientaiDisplay();
+        }
+
+        private void updateKlientas_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Klientas kl = new Klientas();
+                kl.vardas = updateKlientasVardas.Text;
+                kl.pavarde = updateKlientasPav.Text;
+                kl.email = updateKlientasEmail.Text;
+                kl.klientoNr = int.Parse(updateKlientasKlientoNr.Text);
+                
+                usersRep.UpdateKlientas(kl);
+
+                updateKlientasVardas.Clear();
+                updateKlientasPav.Clear();
+                updateKlientasEmail.Clear();
+                updateKlientasKlientoNr.Clear();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            MessageBox.Show("Succesfully updated");
+            getKlientaiDisplay();
+        }
+
+        private void getKlientaiDisplay()
+        {
+            try
+            {
+                //FOR TESTS
+                DataTable dta = usersRep.displayClients();
+                dataGridView1.DataSource = dta;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
     }
 }
